e810b60149a34c238700883cc7cae56cc954e116
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-29 21:24:55 +0800

    feat: some helper functions

---
+++ src/utils.ts
@@ -0,0 +1,25 @@
+import { IToDoItem } from "./App";
+
+interface IObj {
+    [key: string]: IToDoItem;
+}
+
+export const tranArrToObj = (arr: IToDoItem[]): IObj => {
+    return arr.reduce((result: IObj, item) => {
+        result[item.id] = item;
+        return result;
+    }, {});
+}
+
+export const tranObjToArr = (obj: IObj): IToDoItem[] => (
+    Object.keys(obj).map((item) => obj[item])
+)
+
+export const store = (namespace: string, data?: IObj) => {
+    if (data) {
+        return localStorage.setItem(namespace, JSON.stringify(data));
+    }
+
+    const localStore = localStorage.getItem(namespace);
+    return (localStore && JSON.parse(localStore)) || {};
+}
